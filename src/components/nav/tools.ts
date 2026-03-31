c4cc3d485b463bcac94f74bf26e0d96b78dbb6d9
Diego Ramos <54applets_hogs@icloud.com>
2026-03-31 15:27:48 -0400

    add calculator

--- src/components/nav/tools.ts
+++ src/components/nav/tools.ts
@@ -1,4 +1,4 @@
-import { CalculatorIcon } from "lucide-react";
+import { CalculatorIcon, HashIcon } from "lucide-react";
 
 export const TOOLS_NAV = [
   {
@@ -7,5 +7,12 @@ export const TOOLS_NAV = [
     href: "/tools/amortization",
     icon: CalculatorIcon,
     activePatterns: ["/tools/amortization", "/tools/amortization/*"],
+  },
+  {
+    name: "Calculator",
+    description: "Simple arithmetic calculator.",
+    href: "/tools/calculator",
+    icon: HashIcon,
+    activePatterns: ["/tools/calculator", "/tools/calculator/*"],
   },
 ];
